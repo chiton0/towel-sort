d55623b05dd13a8399891820b85da8ca81fdcc47
chiton0 <85007583+chiton0@users.noreply.github.com>
2021-08-21 22:01:46 +0300

    feat: create alghorithm towell sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  if(typeof matrix === 'undefined') return [];
+  matrix.forEach((item, index) => {
+    arr.push(...(index % 2 == 0 ? item : item.reverse()));
+  })
+  return arr;
 }
